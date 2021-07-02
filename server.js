30f35794d52d04e2d6fa39eb6dc3905b57efe919
Simba Pfaira <spfaira@yahoo.com>
2021-07-03 00:00:27 +0200

    Level 1 complete

--- server.js
+++ server.js
@@ -2,22 +2,27 @@
 // This is where your node app starts
 
 //load the 'express' module which makes writing webservers easy
-const express = require("express");
+const express = require('express');
 const app = express();
 
 //load the quotes JSON
-const quotes = require("./quotes.json");
+const quotes = require('./quotes.json');
 
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
-app.get("/", function (request, response) {
+app.get('/', function (request, response) {
   response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
 //START OF YOUR CODE...
-
+app.get('/quotes', function (request, response) {
+  response.status(200).json(quotes);
+});
+app.get('/quotes/random', function (request, response) {
+  response.status(200).json(pickFromArray(quotes));
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -30,5 +35,5 @@ function pickFromArray(arr) {
 
 //Start our server so that it listens for HTTP requests!
 const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port);
 });
